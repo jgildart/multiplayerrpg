fd0464d9e046a819f251bf8a16c87c1ccc153112
Jordan Lin <jordan.lin09@gmail.com>
2016-03-17 20:17:29 -0700

    basic server setup

--- server.js
+++ server.js
@@ -1,2 +1,10 @@
-console.log('im the server!');
-console.log('server is running')
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res){
+  res.send('Server starting');
+});
+
+app.listen(3000, function () {
+  console.log('Server started on port 3000.');
+});
