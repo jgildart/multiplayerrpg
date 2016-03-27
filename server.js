47516fda6942bf24909554319abc9bec4312fe23
Josh Gildart <jgildart@gmail.com>
2016-03-27 12:40:22 -0700

    added redis sessions and required npm modules

--- server.js
+++ server.js
@@ -1,6 +1,32 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 var io = require('socket.io')(http);
+var redis = require('redis');
+var session = require('express-session');
+var RedisStore = require('connect-redis')(session);
+
+// try to load the config
+try {
+  var config = require('./config/dev');
+} catch(e) {
+  throw new Error('You must implement config/dev.js for your local environment');
+}
+
+var redisClient = redis.createClient(config.redis.port, config.redis.host);
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({
+  secret: 'this can be anything! this can even be this!',
+  store: new RedisStore({
+    client: redisClient,
+    prefix: 'game_sess:'
+  }),
+  resave: false,
+  saveUninitialized: false
+}));
 
 app.get('/', function (req, res){
   res.sendFile(__dirname + '/index.html');
